2c826a60417755ba4fdbe62a3e874a2a42856640
Adarsh999bh <adarshbhandary48@gmail.com>
2021-09-23 03:58:45 +0530

    testcases for user registration added

---
+++ exceptions/src/test/java/mood/analyser/UserRegistrationTest.java
@@ -0,0 +1,127 @@
+package mood.analyser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserRegistrationTest {
+    
+    @Test
+	public void firstNameReturnTrueWhenValid() {
+		UserRegistration object = new UserRegistration();
+		try {
+			boolean result = object.validateFirstName("Firstnameexample");
+			assertEquals(true, result);
+		} catch (UserRegistrationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void firstNameReturnFalseWhenInvalid() {
+		UserRegistration object = new UserRegistration();
+		try {
+			boolean result = object.validateFirstName("firstnameexample");
+			assertEquals(true, result);
+		} catch (UserRegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void LastNameReturnTrueWhenValid() {
+		UserRegistration object = new UserRegistration();
+		try {
+			boolean result = object.validateLastName("Lastnameexample");
+			assertEquals(true, result);
+		} catch (UserRegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void LastNameReturnFalseWhenInvalid() {
+		UserRegistration object = new UserRegistration();
+		try {
+			boolean result = object.validateLastName("lastnameexample");
+			assertEquals(true, result);
+		} catch (UserRegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void EmailReturnTrueWhenValid() {
+		UserRegistration object = new UserRegistration();
+		try {
+			boolean result = object.validateEmail("example.correct@mail.com");
+			assertEquals(true, result);
+		} catch (UserRegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void EmailReturnFalseWhenInvalid() {
+		UserRegistration object = new UserRegistration();
+		try {
+			boolean result = object.validateEmail("example.wrong.mail.com");
+			assertEquals(true, result);
+		} catch (UserRegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void PhoneReturnTrueWhenValid() {
+		UserRegistration object = new UserRegistration();
+		try {
+			boolean result = object.validatePhone("911234567890");
+			assertEquals(true, result);
+		} catch (UserRegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void PhoneReturnFalseWhenInvalid() {
+		UserRegistration object = new UserRegistration();
+		try {
+			boolean result = object.validatePhone("123456789X");
+			assertEquals(true, result);
+		} catch (UserRegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void PasswordReturnTrueWhenValid() {
+		UserRegistration object = new UserRegistration();
+		try {
+			boolean result = object.validatePassword("ExampleCorrectPassword123#");
+			assertEquals(true, result);
+		} catch (UserRegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void PasswordReturnFalseWhenInvalid() {
+		UserRegistration object = new UserRegistration();
+		try {
+			boolean result = object.validatePassword("examplewrongpassword");
+			assertEquals(true, result);
+		} catch (UserRegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
